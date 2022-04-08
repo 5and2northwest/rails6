46451565d94bdddc3be871c38497c56674020941
Ethan Clark <eisaiahclark@gmail.com>
2022-04-08 15:52:19 -0700

    installed companies

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_30_180903) do
+ActiveRecord::Schema.define(version: 2022_04_08_224839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name", limit: 150
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "widgets", force: :cascade do |t|
     t.string "name", limit: 150
